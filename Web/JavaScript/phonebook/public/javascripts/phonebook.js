59e4c63b92bc7ac01020d3a1d2458cb02e9df2cb
Igor Mitiukliaev <igor_m@outlook.com>
2019-05-24 17:36:21 +0700

    Phonebook server + complications

--- Web/JavaScript/phonebook/public/javascripts/phonebook.js
+++ Web/JavaScript/phonebook/public/javascripts/phonebook.js
@@ -1,5 +1,7 @@
 Vue.use(VeeValidate, {
-	validity: true
+	validity: true,
+	mode: "lazy",
+	events: 'change'
 });
 
 new Vue({
@@ -13,8 +15,7 @@ new Vue({
 		contacts: [],
 		checkedItems: [],
 		name: null,
-		phone: null,
-		batchDelDisabled: true
+		phone: null
 	},
 
 	created: function () {
@@ -23,86 +24,97 @@ new Vue({
 
 	computed: {
 		classError: function () {
-			var self = this;
-			this.err.name = this.name === "" || this.name === null;
-			this.err.phone = isNaN(this.phone) || this.phone === "" || this.phone === null;
-			return this.err;
+			var error = {};
+			error.name = (this.name === "" || this.name === null);
+			error.phone = (isNaN(this.phone) || this.phone === "" || this.phone === null || this.phone.includes(" "));
+			return error;
+		},
+
+		batchDelDisabled: function () {
+			return this.checkedItems.length === 0;
+		},
+
+		addDisabled: function () {
+			return (this.classError.name || this.classError.phone);
 		}
 	},
 
 	methods: {
-		request: function (url, data, context) {
-			$.post({
+		post: function (url, request) {
+			return $.post({
 				url: url,
-				data: data,
+				data: JSON.stringify(request),
 				contentType: "application/json"
-			}).done(function () {
-				context.loadContacts();
 			});
+		},
+
+		get: function (url, request) {
+			return $.get(url,
+				request);
 		},
 
 		addContact: function () {
 			var self = this;
-			if (this.classError.name||this.classError.phone) {
+			if (this.classError.name || this.classError.phone) {
 				return;
 			}
-
 			var request = {
 				contacts: {
 					name: self.name,
 					phone: self.phone
 				}
 			};
-			this.request(
+
+			this.post(
 				"/addContact",
-				JSON.stringify(request),
-				self);
-			self.name = "";
-			self.phone = "";
-			this.err = {
-				name: false,
-				phone: false
-			}
+				request)
+				.done(function () {
+					self.loadContacts();
+					self.name = "";
+					self.phone = "";
+					this.err = {
+						name: false,
+						phone: false
+					}
+				});
 		},
 
 		deleteContact: function (contact) {
 			var answer = confirm("Delete data?");
 			if (answer) {
 				var self = this;
-				this.request(
+				this.post(
 					"/deleteContact",
-					JSON.stringify({
-						id: contact.id
-					}),
-					self);
+					{id: contact.id},
+				).done(function () {
+					self.loadContacts();
+				});
 			}
 		},
 
 		loadContacts: function (term) {
 			var self = this;
 			self.searchTerm = term;
-			$.get("/getContacts", {term: term}).done(function (contacts) {
-				self.contacts = contacts;
-			});
-		},
-
-		enableBatchDelete: function () {
-			this.batchDelDisabled = this.checkedItems.length === 0;
+			this.get("/getContacts", {term: term})
+				.done(function (contacts) {
+					self.contacts = contacts;
+				});
 		},
 
 		batchDeleteContacts: function (term) {
 			var answer = confirm("Delete data?");
 			if (answer) {
 				var self = this;
-				this.request(
+				this.post(
 					"/batchDeleteContacts",
-					JSON.stringify({
+					{
 						contacts: this.checkedItems,
 						term: term
-					}),
-					self);
+					})
+					.done(function () {
+						self.loadContacts();
+					});
 				this.checkedItems = [];
-				this.batchDelDisabled = true;
 			}
 		}
 	}
