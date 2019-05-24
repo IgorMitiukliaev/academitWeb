71e1194a0499bb8292a2ea56418525436a945999
imitiukl <Pa$$1248>
2019-05-24 22:14:43 +0700

    Phonebook + complications

--- Web/JavaScript/phonebook/public/javascripts/phonebook.js
+++ Web/JavaScript/phonebook/public/javascripts/phonebook.js
@@ -1,121 +1,126 @@
 Vue.use(VeeValidate, {
-	validity: true,
-	mode: "lazy",
-	events: 'change'
+    validity: true
+    /*,
+    mode: "lazy",
+    events: 'change'*/
 });
 
 new Vue({
-	el: "#app",
-	data: {
-		err: {
-			name: false,
-			phone: false
-		},
-		searchTerm: "",
-		contacts: [],
-		checkedItems: [],
-		name: null,
-		phone: null
-	},
+    el: "#app",
+    data: {
+        err: {
+            name: false,
+            phone: false,
+            phoneExists: false
+        },
+        searchTerm: "",
+        contacts: [],
+        checkedItems: [],
+        name: null,
+        phone: null,
+        response: {}
+    },
 
-	created: function () {
-		this.loadContacts();
-	},
+    created: function () {
+        this.loadContacts();
+    },
 
-	computed: {
-		classError: function () {
-			var error = {};
-			error.name = (this.name === "" || this.name === null);
-			error.phone = (isNaN(this.phone) || this.phone === "" || this.phone === null || this.phone.includes(" "));
-			return error;
-		},
+    computed: {
+        classError: function () {
+            var error = {};
+            error.name = (this.name === "" || this.name === null);
+            error.phone = (isNaN(this.phone) || this.phone === "" || this.phone === null || this.phone.indexOf(" ") >= 0);
+            return error;
+        },
 
-		batchDelDisabled: function () {
-			return this.checkedItems.length === 0;
-		},
+        batchDelDisabled: function () {
+            return this.checkedItems.length === 0;
+        },
 
-		addDisabled: function () {
-			return (this.classError.name || this.classError.phone);
-		}
-	},
+        addDisabled: function () {
+            return (this.classError.name || this.classError.phone);
+        }
+    },
 
-	methods: {
-		post: function (url, request) {
-			return $.post({
-				url: url,
-				data: JSON.stringify(request),
-				contentType: "application/json"
-			});
-		},
+    methods: {
+        post: function (url, request) {
+            return $.post({
+                url: url, data: JSON.stringify(request), contentType: "application/json"
+            });
+        },
 
-		get: function (url, request) {
-			return $.get(url,
-				request);
-		},
+        get: function (url, request) {
+            return $.get(url, request);
+        },
 
-		addContact: function () {
-			var self = this;
-			if (this.classError.name || this.classError.phone) {
-				return;
-			}
-			var request = {
-				contacts: {
-					name: self.name,
-					phone: self.phone
-				}
-			};
+        addContact: function () {
+            var self = this;
+            if (this.classError.name || this.classError.phone) {
+                return;
+            }
+            var request = {
+                contacts: {
+                    name: self.name,
+                    phone: self.phone
+                }
+            };
 
-			this.post(
-				"/addContact",
-				request)
-				.done(function () {
-					self.loadContacts();
-					self.name = "";
-					self.phone = "";
-					this.err = {
-						name: false,
-						phone: false
-					}
-				});
-		},
+            var response = this.post("/addContact", request);
+            response.done(function () {
+                self.loadContacts();
+            });
 
-		deleteContact: function (contact) {
-			var answer = confirm("Delete data?");
-			if (answer) {
-				var self = this;
-				this.post(
-					"/deleteContact",
-					{id: contact.id},
-				).done(function () {
-					self.loadContacts();
-				});
-			}
-		},
+            this.err.phoneExists = !response.success;
+            if (this.err.phoneExists) {
+                self.loadContacts();
+            } else {
+                self.name = "";
+                self.phone = "";
+                this.err = {
+                    name: false,
+                    phone: false,
+                    phoneExists: false
+                }
+            }
+        },
 
-		loadContacts: function (term) {
-			var self = this;
-			self.searchTerm = term;
-			this.get("/getContacts", {term: term})
-				.done(function (contacts) {
-					self.contacts = contacts;
-				});
-		},
+        deleteContact: function (contact) {
+            var answer = confirm("Delete data?");
+            if (answer) {
+                var self = this;
+                this.post("/deleteContact", {id: contact.id})
+                    .done(function () {
+                        self.loadContacts();
+                    });
+            }
+        }
+        ,
 
-		batchDeleteContacts: function (term) {
-			var answer = confirm("Delete data?");
-			if (answer) {
-				var self = this;
-				this.post(
-					"/batchDeleteContacts",
-					{
-						contacts: this.checkedItems,
-						term: term
-					})
-					.done(function () {
-						self.loadContacts();
-					});
-				this.checkedItems = [];
-			}
-		}
-	}
-});
+        loadContacts: function (term) {
+            var self = this;
+            self.searchTerm = term;
+            this.get("/getContacts", {term: term})
+                .done(function (contacts) {
+                    self.contacts = contacts;
+                });
+        }
+        ,
+
+        batchDeleteContacts: function (term) {
+            var answer = confirm("Delete data?");
+            if (answer) {
+                var self = this;
+                var request = {
+                    contacts: this.checkedItems,
+                    term: term
+                };
+                this.post("/batchDeleteContacts", request)
+                    .done(function () {
+                        self.loadContacts();
+                    });
+                this.checkedItems = [];
+            }
+        }
+    }
+})
+;
